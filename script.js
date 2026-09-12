fabbd6d7bb66efe1be9c8d60e1a82fc1de8a45ae
Abdul Awal <abdulawal018101@gmail.com>
2026-09-12 05:58:46 +0300

    Update icon for Flag Guess game

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const games = [
     id: 3,
     title: "Flag Guess",
     category: "Quiz",
-    icon: "🌍",
+    icon: "games/file_00000000c8388211815dc83cd8039c22.png",
     url: "games/flag-guess.html",
     newGame: true,
     popular: false
