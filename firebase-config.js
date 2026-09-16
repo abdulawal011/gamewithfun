4a4efe4da09af82b77b70c7fffb7791adabfb69d
Abdul Awal <abdulawal018101@gmail.com>
2026-09-16 23:57:17 +0300

    Update Firebase authentication methods and config

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebas
 import {
   getAuth,
   GoogleAuthProvider,
-  signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
@@ -23,9 +24,8 @@ import {
   getDocs
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBmsDEIWswcmxdDoJuDCMAhM3J3OJQKkG9I",
+  apiKey: "AIzaSyBmsDEIWswcmxdDoJuDCMAh3J3OJQKkG9I",
   authDomain: "game-with-fun-58521.firebaseapp.com",
   projectId: "game-with-fun-58521",
   storageBucket: "game-with-fun-58521.firebasestorage.app",
@@ -34,13 +34,11 @@ const firebaseConfig = {
   measurementId: "G-CRHNTHH34B"
 };
 
-
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
-
 
 export {
   app,
@@ -48,7 +46,8 @@ export {
   db,
   googleProvider,
 
-  signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
   signOut,
   onAuthStateChanged,
 
@@ -58,7 +57,6 @@ export {
   updateDoc,
   increment,
   serverTimestamp,
-
   collection,
   query,
   orderBy,
