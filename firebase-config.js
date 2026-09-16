bf3cb63add7a5164c3d3f989ab0042a8b18a2e2e
Abdul Awal <abdulawal018101@gmail.com>
2026-09-17 00:28:38 +0300

    Update Firebase API key in configuration

--- firebase-config.js
+++ firebase-config.js
@@ -25,7 +25,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmsDEIWswcmxdDoJuDCMAh3J3OJQKkG9I",
+  apiKey: "AIzaSyBmsDEIWswcmxdDoJuDCaMhp3J3OJQKkG9I",
   authDomain: "game-with-fun-58521.firebaseapp.com",
   projectId: "game-with-fun-58521",
   storageBucket: "game-with-fun-58521.firebasestorage.app",
@@ -49,12 +49,10 @@ export {
   auth,
   db,
   googleProvider,
-
   signInWithRedirect,
   getRedirectResult,
   signOut,
   onAuthStateChanged,
-
   doc,
   getDoc,
   setDoc,
