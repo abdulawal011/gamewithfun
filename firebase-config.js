4cbcc72ae314652e3b7ec5cfa3e3d5aba1ded0f7
Abdul Awal <abdulawal018101@gmail.com>
2026-09-17 02:59:07 +0300

    Refactor firebase-config.js to update apiKey
    Updated apiKey in firebaseConfig and removed unused imports and initialization code.

--- firebase-config.js
+++ firebase-config.js
@@ -1,67 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithRedirect,
-  getRedirectResult,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-  increment,
-  serverTimestamp,
-  collection,
-  query,
-  orderBy,
-  limit,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBmsDEIWswcmxdDoJuDCaMhp3J3OJQKkG9I",
+  apiKey: "AIzaSyBmsDEIWswcmxdDoJuDCaMhpJ3OJQKkG9I",
   authDomain: "game-with-fun-58521.firebaseapp.com",
   projectId: "game-with-fun-58521",
   storageBucket: "game-with-fun-58521.firebasestorage.app",
   messagingSenderId: "475244952455",
   appId: "1:475244952455:web:1d6f18b335867c03037122",
   measurementId: "G-CRHNTHH34B"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-const googleProvider = new GoogleAuthProvider();
-
-googleProvider.setCustomParameters({
-  prompt: "select_account"
-});
-
-export {
-  app,
-  auth,
-  db,
-  googleProvider,
-  signInWithRedirect,
-  getRedirectResult,
-  signOut,
-  onAuthStateChanged,
-  doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-  increment,
-  serverTimestamp,
-  collection,
-  query,
-  orderBy,
-  limit,
-  getDocs
 };
